17bcf994bf211c0d16213029c793835c8de14ee3
Lhamjav Sanjmyatav <23LP0694@G304-CC0098.local>
2024-04-03 21:04:11 +0800

    updaetd

--- frontend/src/pages/destination.tsx
+++ frontend/src/pages/destination.tsx
@@ -1,14 +1,37 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { getServerSideProps } from "../../utils/fetchTravelDatas";
+import { Travel } from "../types/travelTypes";
+import { Tours } from "../types/toursTypes";
+import { Destination } from "../types/destinationTypes";
+import { DestinationCategory } from "../types/destinationCategoryTypes";
+import React from "react";
+import { Hero } from "@/components/supports/destinationPage/Hero";
+import { Order } from "@/components/supports/destinationPage/Order";
+import { FormComponent } from "@/components/supports/destinationPage/FormData";
+import { Footer } from "@/components/supports/destinationPage/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Destination() {
-  return (
-    <main
-      className={`flex min-h-screen max-h-screen h-full flex-col w-full items-start justify-start bg-gray-50 ${inter.className}`}
-    >
-
-    </main>
-  );
+export interface Props {
+  travelDatas: Travel
+  toursData: Tours
+  destinationDatas: Destination
+  categoryDatas: DestinationCategory
 }
+
+function Destination({ travelDatas, toursData, destinationDatas, categoryDatas }: Props) {
+  return (
+    <>
+
+      {/* <Order /> */}
+      <Hero toursData={toursData} travelDatas={travelDatas} destinationDatas={destinationDatas} categoryDatas={categoryDatas} />
+      {/* <FormComponent /> */}
+      <Footer />
+    </>
+  );
+};
+
+export { getServerSideProps };
+
+export default Destination;
