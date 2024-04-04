e5eab2f3498b370a4a3f12f9b9f24b3de1e4105c
Dulguun Batbaatar <23LP7194@G304-CC0064.local>
2024-04-04 17:50:55 +0800

    updated 3

---
+++ backdeploy/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (_req: Request, res: Response) => {
+  return res.send("travel backend deploy test");
+});
+
+app.get("/ping", (_req: Request, res: Response) => {
+  return res.send("pong travel back end pong🏓");
+});
+
+app.listen(port, () => {
+  return console.log(`Server is listening on ${port}`);
+});
