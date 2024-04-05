8655ceb27724496901e763fabf865b7525cdbb6d
syerikkh <147494171+syerikkh@users.noreply.github.com>
2024-04-05 17:51:32 +0800

    1 homepage (#13)
    * add homePage and detailPage
    
    * add background image
    
    add background image, installed auth0
    
    * 04.04
    
    * 04.04
    
    * make auth0 work
    
    * fixed auth0
    
    fixed auth0
    
    * add images, login signup
    
    add images, login signup
    
    * added user name besides log out button
    
    added user name besides log out button
    
    ---------
    
    Co-authored-by: 94241021 Khuanysh <23LP2243@G304-CC0080.local>
    Co-authored-by: Dulguun Batbaatar <23LP7194@G304-CC0064.local>

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,13 +1,12 @@
 import { Footer } from "@/components/supports/destinationPage/Footer";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { UserProvider } from '@auth0/nextjs-auth0/client'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-
+    <UserProvider>
       <Component {...pageProps} />
-      <Footer />
-    </>
-  );
+    </UserProvider>
+  )
 }
