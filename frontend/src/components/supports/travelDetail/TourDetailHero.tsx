37d08c07fbdd4b7dbf62819ccace6e2d5fadb109
syerikkh <147494171+syerikkh@users.noreply.github.com>
2024-04-06 13:05:12 +0800

    datas
    datas

--- frontend/src/components/supports/travelDetail/TourDetailHero.tsx
+++ frontend/src/components/supports/travelDetail/TourDetailHero.tsx
@@ -10,7 +10,7 @@ import { FetchDataProps } from '@/types/fetchDataProps';
 
 
 const TourDetailHero = ({ toursData, travelDatas, destinationDatas, categoryDatas }: FetchDataProps) => {
-    const tour = travelDatas.result.filter(travel => travel._id === "66069e7a464834a5e2273c97");
+    const tour = travelDatas.result.filter(travel => travel._id === "660ff3ecfab1c51304eb2117");
     const widerName = travelDatas.result.map((data) => data.name.length > 28)
 
 
