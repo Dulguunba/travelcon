18fb663613266c26bf2d729204fdfbb13a4612b5
Dulguun Batbaatar <23LP7194@G304-CC0064.local>
2024-04-08 19:38:34 +0800

    type error fix

--- frontend/src/components/supports/destinationDetail/TourPackages.tsx
+++ frontend/src/components/supports/destinationDetail/TourPackages.tsx
@@ -1,32 +1,38 @@
-import React from 'react'
-import TourPackageCard from './TourPackageCard'
-import { useRouter } from 'next/router'
-import { Travel } from '@/types/travelTypes';
-import Link from 'next/link';
-import { Loading } from '../Loading';
+import React from "react";
+import TourPackageCard from "./TourPackageCard";
+import { useRouter } from "next/router";
+import { Travel } from "@/types/travelTypes";
+import Link from "next/link";
+import { Loading } from "../Loading";
 
-export const TourPackages = ({ travelDatas, isLoading }: { travelDatas: Travel, isLoading: boolean }) => {
-    const router = useRouter();
-    const { destination } = router.query;
-    if (isLoading) {
-        return <Loading />
-    }
-    return (
-        <div className='flex items-center flex-col mt-20 mb-20'>
-            <h1 className='font-oswald text-3xl lg:text-[40px] font-bold'>AVAILABLE TOUR PACKAGES</h1>
-            <div className='w-full justify-between flex mt-10'>
-                {travelDatas.result.map((data) => (
-                    data.destination.english === destination
-                        ?
-                        <Link href={`/tours/${data._id}`}>
-                            <TourPackageCard image={data.image.mainImage} title={data.name} time={data.duration} destinationName={destination} startPrice={data.price.adultPrice} duration={data.duration} />
-                        </Link>
-                        : null
-                ))}
+export const TourPackages = ({ travelDatas }: { travelDatas: Travel }) => {
+  const router = useRouter();
+  const { destination } = router.query;
 
-
-            </div>
-            <button className='font-primary text-blue text-[20px] font-medium underline mt-10'>See More Packages</button>
-        </div>
-    )
-}
+  return (
+    <div className="flex items-center flex-col mt-20 mb-20">
+      <h1 className="font-oswald text-3xl lg:text-[40px] font-bold">
+        AVAILABLE TOUR PACKAGES
+      </h1>
+      <div className="w-full justify-between flex mt-10">
+        {travelDatas.result.map((data) =>
+          data.destination.english === destination ? (
+            <Link href={`/tours/${data._id}`}>
+              <TourPackageCard
+                image={data.image.mainImage}
+                title={data.name}
+                time={data.duration}
+                destinationName={destination}
+                startPrice={data.price.adultPrice}
+                duration={data.duration}
+              />
+            </Link>
+          ) : null
+        )}
+      </div>
+      <button className="font-primary text-blue text-[20px] font-medium underline mt-10">
+        See More Packages
+      </button>
+    </div>
+  );
+};
